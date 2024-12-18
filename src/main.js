7bf09a18714947004a99174341891d8f39e18b19
psdhtmlcss <psdhtmlcss@mail.ru>
2024-12-18 23:03:45 +0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,45 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const URL = 'http://localhost:3000/modes/';
+const Modes = {
+  HTML: 1,
+  CSS: 2
+};
+const editor = ace.edit('editor');
+const result = document.querySelector('.js-result pre');
+const select = document.querySelector('.js-select-mode');
+const runButton = document.querySelector('.js-run');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+editor.session.setMode('ace/mode/html');
 
-setupCounter(document.querySelector('#counter'))
+function renderResult(data) {
+  result.innerHTML = '';
+  result.insertAdjacentText('afterbegin', data);
+}
+
+function onSelectChange(evt)  {
+  editor.session.setMode(`ace/mode/${evt.target.value}`);
+  editor.setValue('');
+}
+
+function onRunButtonClick(evt) {
+  evt.preventDefault();
+  const data = {
+    language: select[select.selectedIndex].value,
+    code: editor.getValue()
+  };
+  runButton.disabled = true;
+  fetch(`${URL}${Modes[select[select.selectedIndex].value.toUpperCase()]}`, {
+    method: 'put',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(results => renderResult(results.code))
+    .catch((error) => alert(error))
+    .finally(() => runButton.disabled = false)
+}
+
+select.addEventListener('change', onSelectChange);
+runButton.addEventListener('click', onRunButtonClick);
+
